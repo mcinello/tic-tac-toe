9a01bfee2d6602bc1b4d4b9ce9fc9beb8cb5ac65
Michelle Cinello <m.cinello@gmail.com>
2017-11-14 17:32:55 -0500

    storing moves in array

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
     [3, 5, 7]
   ]
 
+  var moves = [0,0,0,0,0,0,0,0,0]
+
+  var cells = document.querySelectorAll('td');
+
   // when player 1 clicks, add x to td
   // can't click in that area anymore
   // change player to 2
@@ -23,25 +27,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // if div index+1 === winningcombos
 
-  var cells = document.querySelectorAll('td');
+
+
+
+  // CHOOSING A CELL, SWAPPING BETWEEN PLAYERS
 
   cells.forEach(function(cell) {
     cell.addEventListener('click', function(e){
-
       if (player === 1 && cell.className === 'cell empty') {
         cell.innerHTML = 'X';
         player = 2;
+        moves[Number(e.target.id)] = "X";
+        console.log(moves);
         ++count;
+        // function here to check win
         cell.className = "cell full";
       } else if (player === 2 && cell.className === 'cell empty') {
         cell.innerHTML = 'O';
         player = 1;
+        moves[Number(e.target.id)] = "0";
+        console.log(moves);
         ++count;
         cell.className = "cell full";
       }
-
-
-
     });
   });
 
