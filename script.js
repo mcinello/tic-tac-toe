335ac8e911f30c073a7142aad9b30d14ff73a2c5
Michelle Cinello <m.cinello@gmail.com>
2017-11-14 16:48:42 -0500

    set up basic template for tic tac toe game

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var count = 0, player = 1
+  var winningCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7]
+  ]
+
+  // when player 1 clicks, add x to td
+  // change player to 2
+  // count ++
+
+  //when player 2 clicks, add o to td
+  // change player to 1
+  // count ++
+
+  var cells = document.querySelectorAll('td');
+
+  cells.forEach(function(cell) {
+
+
+
+
+
+
+  });
+  
+}
